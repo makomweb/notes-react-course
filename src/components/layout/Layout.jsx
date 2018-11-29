ac7c98aaf11f50a366f55e77f588d16502836101
Martin Komischke <martin.komischke@outlook.com>
2018-11-29 10:17:14 +0100

    add margin from CSS

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary';
+import styles from './Layout.css';
 
 const layout = (props) => {
     return (
         <Aux>
-            <div>Toolbar, SideDrawer, Backdrop</div>
-            <main>
+            <div>Toolbar, SideDrawer, Backdrop, Hmm</div>
+            <main className={styles.Content}>
                 {props.children}
             </main>
         </Aux>
