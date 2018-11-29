f75fa175f0d0454108976aa81644b91c842120e7
Martin Komischke <martin.komischke@outlook.com>
2018-11-29 10:08:06 +0100

    add skeleton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Layout from './components/layout/Layout';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Layout>
+          <p>Test</p>
+        </Layout>
       </div>
     );
   }
