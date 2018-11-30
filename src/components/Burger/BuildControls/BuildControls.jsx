89c07938ddb0a7a1d211fc09355abea5c966cb6a
Martin Komischke <martin.komischke@outlook.com>
2018-11-30 15:14:00 +0100

    use static build-controls to build a burger

--- src/components/Burger/BuildControls/BuildControls.jsx
+++ src/components/Burger/BuildControls/BuildControls.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import styles from './BuildControls.css'
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    { label: 'Lettuce', type: 'lettuce' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Patty', type: 'patty' }
+];
 
 const buildControls = (props) => (
     <div className={styles.BuildControls}>
-
+        {
+            controls.map(obj => (
+                <BuildControl key={obj.label} label={obj.label} />
+            ))
+        }
     </div>
 );
 
