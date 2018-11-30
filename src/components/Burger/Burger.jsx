efa234c09cb8d0195f89143c89bf26f565640e05
Martin Komischke <martin.komischke@outlook.com>
2018-11-30 10:28:31 +0100

    assemble the first burger

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import styles from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
     return (
-        <div>
-            
+        <div className={styles.Burger}>
+            <BurgerIngredient type="bun-top" />
+            <BurgerIngredient type="cheese" />
+            <BurgerIngredient type="patty" />
+            <BurgerIngredient type="bun-bottom" />
         </div>
     );
 }
