e1ce58d9a676c553f4f3fca9c9c21ce723994e4e
Martin Komischke <martin.komischke@outlook.com>
2018-11-30 10:05:00 +0100

    fix typo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import Layout from './components/layout/Layout';
-import BurgerBulder from './containers/BurgerBuilder/BurgerBuilder';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBulder/>
+          <BurgerBuilder />
         </Layout>
       </div>
     );
