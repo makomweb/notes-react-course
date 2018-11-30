3fb5c65d81a77009b60466990b8674e186114679
Martin Komischke <martin.komischke@outlook.com>
2018-11-30 10:52:02 +0100

    convert the burger ingredients into an array of burger ingredient components

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -3,11 +3,21 @@ import styles from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+
+    let ingredients = props.ingredients;
+
+    // convert the composite object (key + value) into an array of ingredient components
+    const burgerIngredients = Object.keys(ingredients)
+        .map(ingredientKey => {
+            return [...Array(ingredients[ingredientKey])].map((_, i) => {
+                return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
+            });
+        });
+
     return (
         <div className={styles.Burger}>
             <BurgerIngredient type="bun-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="patty" />
+            {burgerIngredients}
             <BurgerIngredient type="bun-bottom" />
         </div>
     );
