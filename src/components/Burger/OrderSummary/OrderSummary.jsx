256ba672e3bb532b6720912b0e36c859d31289e1
Martin Komischke <martin.komischke@outlook.com>
2018-12-03 10:17:30 +0100

    show the modal with the order summary

---
+++ src/components/Burger/OrderSummary/OrderSummary.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Aux from '../../../hoc/Auxiliary';
+
+const orderSummary = (props) => {
+    const summary = Object.keys(props.ingredients)
+        .map(igKey => {
+            return (
+                <li>
+                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
+                </li>);
+        });
+
+    return (
+        <Aux>
+            <h3>Your order</h3>
+            <p>A delicious burger with the following ingredients:</p>
+            <ul>{summary}</ul>
+            <p>Continue with checkout.</p>
+        </Aux>
+    );
+}
+
+export default orderSummary;
