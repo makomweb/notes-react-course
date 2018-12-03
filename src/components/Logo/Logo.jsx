dd8fc83dc472439dbf405045eb63c4cd68aea3d6
Martin Komischke <martin.komischke@outlook.com>
2018-12-03 11:12:05 +0100

    style the logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import burgerLogo from '../../assets/images/burger-logo.png';
+import styles from './Logo.css';
 
 const logo = (props) => (
-    <div>
+    <div className={styles.Logo}>
         <img src={burgerLogo} />
     </div>
 );
