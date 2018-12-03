275532062d1db163d2b409ee093e26d0e0bd83cc
Martin Komischke <martin.komischke@outlook.com>
2018-12-03 12:13:35 +0100

    turn layout into a stateful component

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Auxiliary';
 import styles from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => {
-    return (
-        <Aux>
-            <Toolbar />
-            <SideDrawer />
-            <main className={styles.Content}>
-                {props.children}
-            </main>
-        </Aux>
-    );
+class Layout extends Component {
+    render() {
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDrawer />
+                <main className={styles.Content}>
+                    {this.props.children}
+                </main>
+            </Aux >
+        );
+    }
 };
 
-export default layout;
+export default Layout;
