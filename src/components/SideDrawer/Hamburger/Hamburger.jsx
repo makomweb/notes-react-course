3233855c248685156a8ddad2140a51573165b1d0
Martin Komischke <martin.komischke@outlook.com>
2018-12-04 11:15:18 +0100

    style Hamburger menu button

--- src/components/SideDrawer/Hamburger/Hamburger.jsx
+++ src/components/SideDrawer/Hamburger/Hamburger.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import styles from './Hamburger.css';
 
 const hamburger = (props) => (
-    <div className={styles.Hamburger} onClick={props.clicked}>MENU</div>
+    <div className={styles.Hamburger} onClick={props.clicked}>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
 );
 
 export default hamburger;
