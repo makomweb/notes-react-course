3f4db0d75fd42083a2a99b4e528512db33a1d580
Martin Komischke <martin.komischke@outlook.com>
2018-12-04 11:32:03 +0100

    [OPTIMIZATION] prevent modal and ordersummary from being rendered before they are actually shown

--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './Modal.css';
 import Aux from '../../../hoc/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-    <Aux>
-        <Backdrop show={props.show} tapped={props.tapped} />
-        <div style={{
-            transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity: props.show ? '1' : '0'
-        }}
-            className={styles.Modal}>
-            {props.children}
-        </div>
-    </Aux>
-);
+class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
 
-export default modal;
+    componentWillUpdate() {
+        console.log("[Modal.js] componentWillUpdate()");
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} tapped={this.props.tapped} />
+                <div style={{
+                    transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: this.props.show ? '1' : '0'
+                }}
+                    className={styles.Modal}>
+                    {this.props.children}
+                </div>
+            </Aux>);
+    }
+}
+
+export default Modal;
