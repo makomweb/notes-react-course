1ff7486fc01f1e636069bc88ca099c64f7eef886
Martin Komischke <martin.komischke@outlook.com>
2018-12-10 16:49:17 +0100

    fix bug where dismissing the error modal does not work when tapping somewhere else

--- src/hoc/ErrorModal/ErrorModal.jsx
+++ src/hoc/ErrorModal/ErrorModal.jsx
@@ -29,7 +29,7 @@ const showErrorModal = (WrappedComponent, axiosInstance) => {
                 <Aux>
                     <Modal
                         show={this.state.error}
-                        clicked={this.errorConfirmedHandler}>
+                        tapped={this.errorConfirmedHandler}>
                         {this.state.error ? this.state.error.message : null}
                     </Modal>
                     <WrappedComponent {...this.props} />
