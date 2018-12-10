829eb637f94dd49b474aa38a2682291fc223a29e
Martin Komischke <martin.komischke@outlook.com>
2018-12-10 16:38:24 +0100

    continue integrating the error modal

--- src/hoc/ErrorModal/ErrorModal.jsx
+++ src/hoc/ErrorModal/ErrorModal.jsx
@@ -3,14 +3,28 @@ import styles from './ErrorModal.css';
 import Modal from '../../components/UI/Modal/Modal';
 import Aux from '../Auxiliary/Auxiliary';
 
-const showErrorModal = (WrappedComponent) => {
+const showErrorModal = (WrappedComponent, axiosInstance) => {
     return class extends Component {
+        state = {
+            error: null
+        }
+
+        componentDidMount() {
+            axiosInstance.interceptors.request.use(request => {
+                this.setState({ error: null });
+            });
+
+            axiosInstance.interceptors.response.use(null, error => {
+                this.setState({ error: error });
+            });
+        }
+        
         render() {
             return (
                 <Aux>
-                    <Modal show>
-                        Something didn't work!
-                </Modal>
+                    <Modal show={this.state.error}>
+                        {this.state.error.message}
+                    </Modal>
                     <WrappedComponent {...this.props} />
                 </Aux>
             );
