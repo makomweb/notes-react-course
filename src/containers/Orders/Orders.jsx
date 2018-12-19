5b0a2305c1324c2885c898b3cde76260fe689190
Martin Komischke <martin.komischke@outlook.com>
2018-12-19 13:20:14 +0100

    fetch orders from the backend

--- src/containers/Orders/Orders.jsx
+++ src/containers/Orders/Orders.jsx
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
 import styles from './Orders.css';
 import Order from '../../components/Order/Order';
+import AxiosInstance from '../../AxiosInstance';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 class Orders extends Component {
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount() {
+        AxiosInstance.get('orders.json')
+            .then(response => {
+                // TURN object
+                // {"-LU5MJeHRdaQwZUb_EIi":{"customer":{"address":{"country":"UK","street":"Bakerstreet 32",
+                // "town":"Smalltown UK"},"name":"DJ Emkay"},"deliveryMethod":"fastest","email":"emaky@test.org",
+                // "ingredients":{"bacon":0,"cheese":2,"lettuce":0,"patty":2},"price":"6.8999999999999995"},
+                // "-LU5MLxBIe2mryYCUwQ_":{"customer":{"address":{"country":"UK","street":"Bakerstreet 32",
+                // "town":"Smalltown UK"},"name":"DJ Emkay"},"deliveryMethod":"fastest","email":"emaky@test.org",
+                // "ingredients":{"bacon":1,"cheese":1,"lettuce":0,"patty":1},"price":"5.9"}}
+                // INTO an ARRAY of ORDER components
+                const orders = [];
+                for (let key in response.data) {
+                    orders.push({
+                        id: key,
+                        ...response.data[key]
+                    });
+                }
+                this.setState({ orders: orders, loading: false });
+            })
+            .catch(error => {
+                console.log(error);
+                this.setState({ loading: false });
+            });
+    }
+
     render() {
+        let content = <div>dummy</div>;
+
+        if (this.state.loading) {
+            content = <Spinner />;
+        }
+
         return (
             <div>
-                <Order />
-                <Order />
+                {content}
             </div>
         );
     }
