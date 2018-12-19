f0e039d9453179fa5af95f6e47b4f204a3074ab5
Martin Komischke <martin.komischke@outlook.com>
2018-12-19 13:06:56 +0100

    cleanup obsolete code

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.jsx
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.jsx
@@ -2,18 +2,15 @@ import React from 'react';
 import styles from './NavigationItem.css';
 import { NavLink } from 'react-router-dom';
 
-const navigationItem = (props) => {
-    console.log(props);
-    return (
-        <li className={styles.NavigationItem}>
-            <NavLink
-                activeClassName={styles.active}
-                to={props.link}
-                exact={props.exact}>
-                {props.children}
-            </NavLink>
-        </li>
-    );
-}
+const navigationItem = (props) => (
+    <li className={styles.NavigationItem}>
+        <NavLink
+            activeClassName={styles.active}
+            to={props.link}
+            exact={props.exact}>
+            {props.children}
+        </NavLink>
+    </li>
+);
 
 export default navigationItem;
