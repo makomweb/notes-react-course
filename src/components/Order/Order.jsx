beb297f144c5524c315558125bea884f0d7c7c55
Martin Komischke <martin.komischke@outlook.com>
2018-12-19 13:39:39 +0100

    alternative approach for price conversion string to decimal

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -4,7 +4,7 @@ import styles from './Order.css';
 const order = (props) => (
     <div className={styles.Order}>
         <p>Ingredients: Lettuce (1)</p>
-        <p>Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong></p>
+        <p>Price: <strong>USD {props.price.toFixed(2)}</strong></p>
     </div>
 );
 
--- src/containers/Orders/Orders.jsx
+++ src/containers/Orders/Orders.jsx
@@ -48,7 +48,7 @@ class Orders extends Component {
                     <Order
                         key={order.id}
                         ingredients={order.ingredients}
-                        price={order.price}
+                        price={+order.price}
                     />
                 )
             });
