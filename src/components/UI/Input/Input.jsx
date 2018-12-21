bfde7cf14d38f8ea67eaf64dd3a53017bafafd2e
Martin Komischke <martin.komischke@outlook.com>
2018-12-21 12:36:51 +0100

    implement Input component

---
+++ src/components/UI/Input/Input.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import styles from './Input.css';
+
+const input = (props) => {
+    let inputElement = null;
+
+    switch (props.inputType) {
+        case ('textarea'):
+            inputElement = <textarea className={styles.Label} {...props} />;
+            break;
+        default:
+        case ('input'):
+            inputElement = <input className={styles.InputElement} {...props} />
+            break;
+    }
+
+    return (
+        <div className={styles.Input}>
+            <label>{props.label}</label>
+            {inputElement}
+        </div>
+    );
+}
+
+export default input;
