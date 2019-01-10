05f9d7c7654ea0912edfca35eb2e87f08984f91d
Martin Komischke <martin.komischke@outlook.com>
2019-01-10 14:03:25 +0100

    enable redux-dev-tools
    (https://github.com/zalmoxisus/redux-devtools-extension)

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux'
 import Reducer from './store/reducer';
 
-const store = createStore(Reducer);
+const store = createStore(Reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const app = (
     <Provider store={store}>
