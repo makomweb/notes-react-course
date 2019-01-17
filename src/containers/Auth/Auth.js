eab342f73febce3bcdf7fe825ec64e382cf4d53d
Martin Komischke <martin.komischke@outlook.com>
2019-01-17 14:40:26 +0100

    wire input validation

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -37,27 +37,56 @@ class Auth extends Component {
         }
     }
 
-    inputChangedHandler = (event, inputIdentifier) => {
-        const updatedOrderForm = {
-            ...this.state.orderForm
+    inputChangedHandler = (event, controlName) => {
+        const { controls } = this.state;
+        const { value } = event.target;
+        const updatedControls = {
+            ...controls,
+            [controlName]: {
+                ...controls[controlName],
+                value: value,
+                valid: this.checkValidity(value, controls[controlName].validation),
+                touched: true
+            }
         };
-        const updatedFormElement = {
-            ...updatedOrderForm[inputIdentifier]
-        };
-        updatedFormElement.value = event.target.value;
-        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
-        updatedFormElement.touched = true;
-        updatedOrderForm[inputIdentifier] = updatedFormElement;
 
-        let formIsValid = true;
-        for (let inputIdentifier in updatedOrderForm) {
-            formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
-        }
-        this.setState({ orderForm: updatedOrderForm, formIsValid: formIsValid });
+
+        this.setState({ controls: updatedControls });
     }
 
     orderHandler = (event) => {
         event.preventDefault();
+    }
+
+    checkValidity(value, rules) {
+        let isValid = true;
+        if (!rules) {
+            return true;
+        }
+
+        if (rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid
+        }
+
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid
+        }
+
+        if (rules.isEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        if (rules.isNumeric) {
+            const pattern = /^\d+$/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        return isValid;
     }
 
     render() {
