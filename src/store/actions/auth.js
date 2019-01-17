c3eb991195fbee5abfca04c51fb780f2b88c0a65
Martin Komischke <martin.komischke@outlook.com>
2019-01-17 14:46:03 +0100

    create actions and action creators for authentication

---
+++ src/store/actions/auth.js
@@ -0,0 +1,30 @@
+import * as actionTypes from './actionTypes';
+
+// action creators for authentication
+
+const authStart = () => {
+    return {
+        type: actionTypes.AUTH_START
+    }
+}
+
+const authSuccess = (authData) => {
+    return {
+        type: actionTypes.AUTH_SUCCESS,
+        authData: authData
+    }
+}
+
+const authFailed = (error) => {
+    return {
+        type: actionTypes.AUTH_FAILED,
+        error: error
+    }
+}
+
+export const auth = (email, password) => {
+    return dispatch => {
+        dispatch(authStart());
+        dispatch.then
+    }
+}
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -3,6 +3,7 @@ export {
     removeIngredient,
     initIngredients
 } from './burgerBuilder';
+
 export {
     purchaseBurger,
     purchaseInit,
