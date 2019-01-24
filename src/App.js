3b261a46ff996a54750dc16134f1a7c06e79da8b
Martin Komischke <martin.komischke@outlook.com>
2019-01-24 14:06:37 +0100

    implement logic for login automatically when the app starts

--- src/App.js
+++ src/App.js
@@ -6,8 +6,14 @@ import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
+import { connect } from 'react-redux';
+import * as actions from './store/actions/index';
 
 class App extends Component {
+  componentDidMount = () => {
+    this.props.tryAutoSignup();
+  }
+
   render() {
     return (
       <div>
@@ -25,4 +31,10 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    tryAutoSignup: () => dispatch(actions.authCheckState())
+  }
+}
+
+export default connect(null, mapDispatchToProps)(App);
