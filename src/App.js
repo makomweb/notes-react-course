818010a0e950f7ecadb1b42c3797339895c66aa0
Martin Komischke <martin.komischke@outlook.com>
2019-01-24 16:35:01 +0100

    lazy load the orders component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 import { connect } from 'react-redux';
 import * as actions from './store/actions/index';
+
+const Orders = React.lazy(() => import('./containers/Orders/Orders'));
 
 class App extends Component {
   componentDidMount = () => {
@@ -27,7 +28,12 @@ class App extends Component {
       routes = (
         <Switch>
           <Route path="/checkout" component={Checkout} />
-          <Route path="/orders" component={Orders} />
+          <Route path="/orders" render={() => (
+            <Suspense fallback={<div>Loading...</div>}>
+              <Orders />
+            </Suspense>
+          )}>
+          </Route>
           <Route path="/logout" component={Logout} />
           <Route path="/auth" component={Auth} />
           <Route path="/" exact component={BurgerBuilder} />
