f6c7d7caeac08a45d1d045ca7a31f20cdca156d6
Martin Komischke <martin.komischke@outlook.com>
2019-01-25 13:38:33 +0100

    simplify reducer test

--- src/store/reducers/auth.test.js
+++ src/store/reducers/auth.test.js
@@ -1,14 +1,9 @@
-import reducer from './auth';
+import reducer, { initialState } from './auth';
 import * as actionTypes from '../actions/actionTypes';
 
 describe('auth reducer', () => {
-    it('should return the initial state', () => {
-        expect(reducer(undefined, {})).toEqual({
-            token: null,
-            userId: null,
-            error: null,
-            loading: false,
-            authRedirect: "/"
-        });
+    it('should return the initial state!', () => {
+        expect(reducer(undefined, {})).toEqual(initialState);
+    });
     });
 });
