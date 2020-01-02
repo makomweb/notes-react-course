b0b6673780da0336077fe11c16efbd3fbb1e1af6
Martin Komischke <itl305@ecointense.net>
2020-01-02 15:47:44 +0100

    fix warning for unused code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Suspense } from 'react';
+import React, { Component } from 'react';
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
