a8aac474d4db7d44d551815b7ba8ef15fad066b8
Martin Komischke <martin.komischke@outlook.com>
2020-11-25 14:35:15 +0100

    use map() to render the array of persons

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -51,17 +51,13 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age} />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={() => this.switchNameHandler('Arnold')}
-            changed={this.nameChangedHandler} >My hobbies: parachute</Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age} />
+          {this.state.persons.map(p => {
+            return (
+              <Person
+                name={p.name}
+                age={p.age} />
+            );
+          })}
         </div>
       );
     }
