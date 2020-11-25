affbeac5997f315b84c7d925db6c0845657dbc7e
Martin Komischke <martin.komischke@outlook.com>
2020-11-25 07:53:47 +0100

    fix new lines

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -5,10 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>
-          Hi, I'm a React App
-        </h1>
-
+        <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
       </div>
     );
