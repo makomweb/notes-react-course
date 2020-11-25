67af5bda411fc6fe56dac6f413452eb73b0e0511
Martin Komischke <martin.komischke@outlook.com>
2020-11-25 08:12:27 +0100

    use state to pass values to the Person components

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,14 +3,23 @@ import './App.css';
 import Person from './Person/Person.js';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Peter', age: 19 },
+      { name: 'Paul', age: 20 },
+      { name: 'Mary', age: 21 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <Person name="Peter" age="21" />
-        <Person name="Paul" age="20">My hobbies: parachute</Person>
-        <Person name="Mary" age="19" />
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My hobbies: parachute</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     //return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hi, I\'m a React App'));
