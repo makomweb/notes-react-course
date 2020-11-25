6be76f36fdd54056395961cf5a3d638b3e39bb3c
Martin Komischke <martin.komischke@outlook.com>
2020-11-25 16:51:17 +0100

    delete a person when clicked on the paragraph

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -4,8 +4,8 @@ import './Person.css';
 const Person = (props) => {
     const { age, name } = props;
     return (
-        <div className="Person" onClick={props.click}>
-            <p>I'm a {name} an I am {age} years old.</p>
+        <div className="Person" >
+            <p onClick={props.click}>I'm a {name} an I am {age} years old.</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
         </div>
