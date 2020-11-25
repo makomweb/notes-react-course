2cd3435318a732da4bed2d4212e615a6dec4e6ec
Martin Komischke <martin.komischke@outlook.com>
2020-11-25 08:03:35 +0100

    parametrize Person component using the Spread operator

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const person = () => {
-    const age = Math.floor(Math.random() * 30);
-    return <p>I'm a person an I am {age} years old.</p>
+const person = (props) => {
+    const { age, name } = props;
+    return <p>I'm a {name} an I am {age} years old.</p>
 }
 
 export default person;
