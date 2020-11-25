4135f32e2ef372f1752785000415fa0b72c9ee8a
Martin Komischke <martin.komischke@outlook.com>
2020-11-25 08:05:31 +0100

    also render the child elements of the Person component

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 const person = (props) => {
     const { age, name } = props;
-    return <p>I'm a {name} an I am {age} years old.</p>
+    return (
+        <div>
+            <p>I'm a {name} an I am {age} years old.</p>
+            <p>{props.children}</p>
+        </div>
+    );
 }
 
 export default person;
