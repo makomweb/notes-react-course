7e2e4e87b57fabb54dac52fc6f34df9481d84181
Martin Komischke <martin.komischke@outlook.com>
2020-11-26 11:40:27 +0100

    prepare for using 'media-queries' from within inline styles

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Person.css';
+import Radium from 'radium';
 
 const Person = (props) => {
+    const style = {
+        '@media (min-width: 500px)': {
+            width: '450px'
+        }
+    }
     const { age, name } = props;
     return (
-        <div className="Person" >
+        <div className="Person" style={style} >
             <p onClick={props.click}>I'm a {name} an I am {age} years old.</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
@@ -12,4 +18,4 @@ const Person = (props) => {
     );
 }
 
-export default Person;
+export default Radium(Person);
