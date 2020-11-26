a655196c5a217404e5b32a52917087e7b838d0a3
Martin Komischke <martin.komischke@outlook.com>
2020-11-26 11:36:44 +0100

    use the pseude selector ':hover' in the inline style

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -47,7 +47,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -67,6 +71,10 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     let classes = [];
