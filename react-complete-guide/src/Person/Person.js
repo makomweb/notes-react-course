b7a85a84d1c48b901e2e1b7bb46ba13d26d9a099
Martin Komischke <martin.komischke@outlook.com>
2020-11-26 16:56:33 +0100

    use scoped CSS styles for the Person component

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './Person.css';
+import classes from './Person.css';
 
 const Person = (props) => {
     const { age, name } = props;
     return (
-        <div className="Person" >
+        <div className={classes.Person} >
             <p onClick={props.click}>I'm a {name} an I am {age} years old.</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
