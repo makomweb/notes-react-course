ab5c4bfcbd2ebd02edc26c2c0b3a06cacf1507ce
Martin Komischke <martin.komischke@outlook.com>
2020-11-26 20:43:01 +0100

    construct an artifical error scenario

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -2,6 +2,12 @@ import React from 'react';
 import './Person.css';
 
 const Person = (props) => {
+
+    const rnd = Math.random();
+    if (rnd > 0.7) {
+        throw new Error('Something went wrong!')
+    }
+
     const { age, name } = props;
     return (
         <div className="Person" >
