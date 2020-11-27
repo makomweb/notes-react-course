1a84f5f509a0c14e8072ecf9c24ddc7a29d05411
Martin Komischke <martin.komischke@outlook.com>
2020-11-27 14:27:04 +0100

    turn the 2 functional components into class based components

--- react-complete-guide/src/Components/Persons/Person/Person.js
+++ react-complete-guide/src/Components/Persons/Person/Person.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Person.css';
 
-const Person = (props) => {
-    const { age, name } = props;
-    return (
-        <div className={classes.Person} >
-            <p onClick={props.click}>I'm a {name} an I am {age} years old.</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name} />
-        </div>
-    );
+class Person extends Component {
+    render() {
+        const { age, name, click, children, changed } = this.props;
+        return (
+            <div className={classes.Person} >
+                <p onClick={click}>I'm a {name} an I am {age} years old.</p>
+                <p>{children}</p>
+                <input type="text" onChange={changed} value={name} />
+            </div>
+        );
+    }
 }
 
 export default Person;
--- react-complete-guide/src/Components/Persons/Persons.js
+++ react-complete-guide/src/Components/Persons/Persons.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person.js'
 
-const Persons = (props) => (
-    props.persons
-        .map((person, index) =>
-            <Person
-                key={person.id}
-                name={person.name}
-                age={person.age}
-                click={props.clicked.bind(this, index)}
-                changed={(event) => props.changed(event, person.id)} />
-        )
-);
+class Persons extends Component {
+    render() {
+        const { props } = this;
+        return props.persons
+            .map((person, index) =>
+                <Person
+                    key={person.id}
+                    name={person.name}
+                    age={person.age}
+                    click={props.clicked.bind(this, index)}
+                    changed={(event) => props.changed(event, person.id)} />
+            );
+    }
+}
 
 export default Persons;
