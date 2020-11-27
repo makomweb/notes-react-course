327db382bde53145d21ebf040e945a961a4c4b72
Martin Komischke <martin.komischke@outlook.com>
2020-11-27 20:55:51 +0100

    use PureComponent to achieve the same without implementing the check

--- react-complete-guide/src/Components/Persons/Persons.js
+++ react-complete-guide/src/Components/Persons/Persons.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import Person from './Person/Person.js'
 
-class Persons extends Component {
+class Persons extends PureComponent {
     state = {};
     constructor(props) {
         super(props);
@@ -29,15 +29,15 @@ class Persons extends Component {
         return { message: 'Payload!' };
     }
 
-    shouldComponentUpdate(nextProps, nextState) {
-        console.log('[Persons.js] shouldComponentUpdate()');
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     console.log('[Persons.js] shouldComponentUpdate()');
 
-        return (
-            nextProps.persons !== this.props.persons ||
-            nextProps.changed !== this.props.changed ||
-            nextProps.clicked !== this.props.clicked
-        );
-    }
+    //     return (
+    //         nextProps.persons !== this.props.persons ||
+    //         nextProps.changed !== this.props.changed ||
+    //         nextProps.clicked !== this.props.clicked
+    //     );
+    // }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
         console.log('[Persons.js] componentDidUpdate()');
