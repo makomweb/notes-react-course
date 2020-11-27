d7dff7b9d4c7cc23cc1ed75b806501779168d679
Martin Komischke <martin.komischke@outlook.com>
2020-11-27 20:54:11 +0100

    extend optimization check

--- react-complete-guide/src/Components/Persons/Persons.js
+++ react-complete-guide/src/Components/Persons/Persons.js
@@ -31,7 +31,12 @@ class Persons extends Component {
 
     shouldComponentUpdate(nextProps, nextState) {
         console.log('[Persons.js] shouldComponentUpdate()');
-        return nextProps.persons !== this.props.persons;
+
+        return (
+            nextProps.persons !== this.props.persons ||
+            nextProps.changed !== this.props.changed ||
+            nextProps.clicked !== this.props.clicked
+        );
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
