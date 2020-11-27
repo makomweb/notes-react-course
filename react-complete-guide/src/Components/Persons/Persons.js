0ddbd14c03710f83fda9b3bf72374b0f5be10d53
Martin Komischke <martin.komischke@outlook.com>
2020-11-27 20:38:18 +0100

    only re-render the persons if they actually changed

--- react-complete-guide/src/Components/Persons/Persons.js
+++ react-complete-guide/src/Components/Persons/Persons.js
@@ -31,7 +31,7 @@ class Persons extends Component {
 
     shouldComponentUpdate(nextProps, nextState) {
         console.log('[Persons.js] shouldComponentUpdate()');
-        return true;
+        return nextProps.persons !== this.props.persons;
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
