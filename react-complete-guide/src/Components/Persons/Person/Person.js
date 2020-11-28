d763785960f2a4bfdabc208a32b8f87344a9212a
Martin Komischke <martin.komischke@outlook.com>
2020-11-28 09:53:30 +0100

    add prop-types to the Person component

--- react-complete-guide/src/Components/Persons/Person/Person.js
+++ react-complete-guide/src/Components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import classes from './Person.css';
 import WithClass from '../../../HOC/WithClass.js';
+import PropTypes from 'prop-types';
 
 class Person extends Component {
     state = {};
@@ -51,5 +52,12 @@ class Person extends Component {
         );
     }
 }
+
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
+};
 
 export default WithClass(Person, classes.Person);
