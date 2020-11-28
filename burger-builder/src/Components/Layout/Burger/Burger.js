51b87fbea67955f8e2d3414da3ebd96800e2f2a5
Martin Komischke <martin.komischke@outlook.com>
2020-11-28 17:05:18 +0100

    show 'please start adding ...' when there are no ingredients

--- burger-builder/src/Components/Layout/Burger/Burger.js
+++ burger-builder/src/Components/Layout/Burger/Burger.js
@@ -3,7 +3,7 @@ import classes from './Burger.css';
 import Ingredient from './Ingredient/Ingredient.js';
 
 const Burger = props => {
-    const ingredients = Object.keys(props.ingredients)
+    let ingredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <Ingredient key={igKey + i} type={igKey} />
@@ -11,7 +11,12 @@ const Burger = props => {
         })
         .reduce((arr, elem) => {
             return arr.concat(elem);
-        }), [];
+        }, []);
+
+
+    if (ingredients.length === 0) {
+        ingredients = <p>Please start adding ingredients!</p>
+    }
 
     return (
         <div className={classes.Burger}>
