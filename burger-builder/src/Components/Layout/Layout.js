95c8080433059c4f66575eef4c46ace5c2c0a3ef
Martin Komischke <martin.komischke@outlook.com>
2020-11-28 16:15:49 +0100

    add Layout and Auxiliary components

---
+++ burger-builder/src/Components/Layout/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import React from 'react';
+import Auxiliary from '../../HOC/Auxiliary';
+
+const Layout = props => {
+    return (
+        <Auxiliary>
+            <div>
+                Toobar, Sidedrawer, Backdrop
+            </div>
+            <main>
+                {props.children}
+            </main>
+        </Auxiliary>
+    );
+}
+
+export default Layout;
