88c15b8b0bc0a3fde323c06d5e329e85ef5b11fc
Martin Komischke <martin.komischke@outlook.com>
2020-11-28 16:21:59 +0100

    introduce BurgerBuilder component

---
+++ burger-builder/src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class BurgerBuilder extends Component {
+
+    render() {
+        return (
+            <div>
+                Burger Builder!
+            </div>
+        )
+    }
+}
+
+export default BurgerBuilder;
