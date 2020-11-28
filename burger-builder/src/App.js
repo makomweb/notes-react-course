f2db6ee4adadefc47d2c34e8d4c69bc4e977e7c6
Martin Komischke <martin.komischke@outlook.com>
2020-11-28 16:23:26 +0100

    use the BurgerBuilder component

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Layout from './Components/Layout/Layout';
+import BurgerBuilder from './Containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
       <Layout>
-        Test
+        <BurgerBuilder />
       </Layout>
     );
   }
--- burger-builder/src/Containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import Auxiliary from '../../HOC/Auxiliary';
 
 class BurgerBuilder extends Component {
 
     render() {
         return (
-            <div>
-                Burger Builder!
-            </div>
+            <Auxiliary>
+                <div>Burger (graphical representation)</div>
+                <div>Build controls</div>
+            </Auxiliary>
         )
     }
 }
