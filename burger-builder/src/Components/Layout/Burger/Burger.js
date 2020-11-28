05d74d0ce15ae684c9160b74037b06c6686fb1e8
Martin Komischke <martin.komischke@outlook.com>
2020-11-28 16:58:57 +0100

    transform object of ingredients into an array of ingredient components

--- burger-builder/src/Components/Layout/Burger/Burger.js
+++ burger-builder/src/Components/Layout/Burger/Burger.js
@@ -3,11 +3,17 @@ import classes from './Burger.css';
 import Ingredient from './Ingredient/Ingredient.js';
 
 const Burger = props => {
+    const ingredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <Ingredient key={igKey + i} type={igKey} />
+            });
+        });
+
     return (
         <div className={classes.Burger}>
             <Ingredient type="bread-top" />
-            <Ingredient type="cheese" />
-            <Ingredient type="beef" />
+            {ingredients}
             <Ingredient type="bread-bottom" />
         </div>
     );
