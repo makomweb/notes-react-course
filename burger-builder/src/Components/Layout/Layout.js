7dbc1d20008f7553e477a1ccfff6a7824c1d63b3
Martin Komischke <martin.komischke@outlook.com>
2020-11-29 22:59:23 +0100

    use the Toolbar

--- burger-builder/src/Components/Layout/Layout.js
+++ burger-builder/src/Components/Layout/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Auxiliary from '../../HOC/Auxiliary';
+import Toolbar from '../Navigation/Toolbar/Toolbar.js';
 import classes from './Layout.css';
 
 const Layout = props => {
     return (
         <Auxiliary>
+            <Toolbar />
             <div>
-                Toobar, Sidedrawer, Backdrop
+                Sidedrawer
             </div>
             <main className={classes.Content}>
                 {props.children}
