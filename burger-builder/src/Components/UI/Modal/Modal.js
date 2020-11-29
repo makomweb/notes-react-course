274a34fbaaaa6b1beefbab39e2279f487ce1a0bc
Martin Komischke <martin.komischke@outlook.com>
2020-11-29 18:21:50 +0100

    introduce multiple lines

--- burger-builder/src/Components/UI/Modal/Modal.js
+++ burger-builder/src/Components/UI/Modal/Modal.js
@@ -2,7 +2,12 @@ import React from 'react';
 import classes from './Modal.css';
 
 const Modal = props => {
-    return <div className={classes.Modal}>{props.children}</div>
+    return (
+        <div
+            className={classes.Modal}>
+            {props.children}
+        </div>
+    );
 }
 
 export default Modal;
