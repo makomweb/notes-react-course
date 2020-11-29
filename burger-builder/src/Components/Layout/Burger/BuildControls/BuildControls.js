0bf3f008012cc2811ea24a4e20ac0cbdc98d5f6b
Martin Komischke <martin.komischke@outlook.com>
2020-11-29 09:45:41 +0100

    show the total price in the burger controls

--- burger-builder/src/Components/Layout/Burger/BuildControls/BuildControls.js
+++ burger-builder/src/Components/Layout/Burger/BuildControls/BuildControls.js
@@ -13,6 +13,7 @@ const BuildControls = props => {
 
     return (
         <div className={classes.BuildControls}>
+            <p>Current price: <strong>{props.price.toFixed(2)}</strong></p>
             {
                 controls.map(c => {
                     return <BuildControl
