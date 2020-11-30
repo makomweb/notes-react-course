5a1b04617d33da348b2fabc0cc3f30720b92d13b
Martin Komischke <martin.komischke@outlook.com>
2020-11-30 15:25:50 +0100

    hide the Toolbar navigation items when in mobile mode

--- burger-builder/src/Components/Navigation/Toolbar/Toolbar.js
+++ burger-builder/src/Components/Navigation/Toolbar/Toolbar.js
@@ -11,7 +11,7 @@ const Toolbar = () => (
         <div className={classes.Logo}>
             <Logo />
         </div>
-        <nav>
+        <nav className={classes.DesktopOnly} >
             <NavigationItems />
         </nav>
     </header>
