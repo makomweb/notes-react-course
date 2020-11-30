9b5317e114eae0a15d518faa8e366608309448f6
Martin Komischke <martin.komischke@outlook.com>
2020-11-30 15:50:14 +0100

    make the SideDrawer disappear when the backdrop is tapped

--- burger-builder/src/Components/Navigation/SideDrawer/SideDrawer.js
+++ burger-builder/src/Components/Navigation/SideDrawer/SideDrawer.js
@@ -5,10 +5,13 @@ import Backdrop from '../../UI/Backdrop/Backdrop.js';
 import classes from './SideDrawer.css';
 
 const SideDrawer = props => {
+    const attachedClasses = [classes.SideDrawer];
+    attachedClasses.push(props.open ? classes.Open : classes.Close);
+
     return (
         <Fragment>
             <Backdrop show={props.open} tapped={props.closed} />
-            <div className={classes.SideDrawer}>
+            <div className={attachedClasses.join(' ')}>
                 <div className={classes.Logo}>
                     <Logo />
                 </div>
