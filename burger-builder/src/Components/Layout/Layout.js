34951cbd1c52dfc26d3452fc5d671853eb26bd08
Martin Komischke <martin.komischke@outlook.com>
2020-11-30 15:41:18 +0100

    make Layout a class based component

--- burger-builder/src/Components/Layout/Layout.js
+++ burger-builder/src/Components/Layout/Layout.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Auxiliary from '../../HOC/Auxiliary';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import Toolbar from '../Navigation/Toolbar/Toolbar.js';
 import classes from './Layout.css';
 
-const Layout = props => {
-    return (
-        <Auxiliary>
-            <Toolbar />
-            <SideDrawer />
-            <main className={classes.Content}>
-                {props.children}
-            </main>
-        </Auxiliary>
-    );
+class Layout extends Component {
+    render() {
+        return (
+            <Auxiliary>
+                <Toolbar />
+                <SideDrawer />
+                <main className={classes.Content}>
+                    {props.children}
+                </main>
+            </Auxiliary>
+        );
+    }
 }
 
 export default Layout;
