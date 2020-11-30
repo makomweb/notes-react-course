bb80f49043e53cb2cb125387d04e975a074dfa2a
Martin Komischke <martin.komischke@outlook.com>
2020-11-30 16:48:01 +0100

    see that the Modal is also updated when adding ingredients, though it is not shown

--- burger-builder/src/Components/UI/Modal/Modal.js
+++ burger-builder/src/Components/UI/Modal/Modal.js
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Auxiliary from '../../../HOC/Auxiliary.js';
 import Backdrop from '../Backdrop/Backdrop.js';
 import classes from './Modal.css';
 
-const Modal = props => {
-    return (
-        <Auxiliary>
-            <Backdrop show={props.show} tapped={props.tapped} />
-            <div
-                className={classes.Modal}
-                style={{
-                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                    opacity: props.show ? '1' : '0'
-                }}>
-                {props.children}
-            </div>
-        </Auxiliary>
-    );
+class Modal extends Component {
+    componentDidUpdate = () => {
+        console.log('[Modal.js] componentDidUpdate();')
+    }
+
+    render() {
+        const { props } = this;
+        return (
+            <Auxiliary>
+                <Backdrop show={props.show} tapped={props.tapped} />
+                <div
+                    className={classes.Modal}
+                    style={{
+                        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: props.show ? '1' : '0'
+                    }}>
+                    {props.children}
+                </div>
+            </Auxiliary>
+        );
+    }
 }
 
 export default Modal;
