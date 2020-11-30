97616d4d445a88a41087e34436d89ef496635637
Martin Komischke <martin.komischke@outlook.com>
2020-11-30 15:39:05 +0100

    show the Backdrop when the SideDrawer is active

--- burger-builder/src/Components/Navigation/SideDrawer/SideDrawer.js
+++ burger-builder/src/Components/Navigation/SideDrawer/SideDrawer.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop.js';
 import classes from './SideDrawer.css';
 
 const SideDrawer = () => {
     return (
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo />
+        <Fragment>
+            <Backdrop show />
+            <div className={classes.SideDrawer}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </Fragment>
     );
 };
 
