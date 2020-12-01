e9850a1800c8a01e4c4567c30dea7c00dac2e874
Martin Komischke <martin.komischke@outlook.com>
2020-12-01 14:59:39 +0100

    add axios interceptors

--- starting-project/src/index.js
+++ starting-project/src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+axios.interceptors.request.use(requestConfig => {
+    console.log('Request config: ', requestConfig);
+    // customize request config here!
+    return requestConfig;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(responseConfig => {
+    console.log('Response config: ', responseConfig);
+    return responseConfig;
+
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
