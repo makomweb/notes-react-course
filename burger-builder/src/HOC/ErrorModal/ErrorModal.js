b1ef2c515b0c774048005aee776324011bddeb63
Martin Komischke <martin.komischke@outlook.com>
2020-12-02 08:19:16 +0100

    return a class based component when showing the error modal

--- burger-builder/src/HOC/ErrorModal/ErrorModal.js
+++ burger-builder/src/HOC/ErrorModal/ErrorModal.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Modal from '../../Components/UI/Modal/Modal.js'
 import Aux from '../Auxiliary/Auxiliary.js';
 
 const ErrorModal = (WrappedComponent) => {
-    return (props) => {
-        return (
-            <Aux>
-                <Modal show>
-                    Something didn't work!
-                </Modal>
-                <WrappedComponent {...props} />
-            </Aux>
-        );
+    return class extends Component {
+        render() {
+            return (props) => {
+                const { props } = this;
+                return (
+                    <Aux>
+                        <Modal show>
+                            Something didn't work!
+                        </Modal>
+                        <WrappedComponent {...props} />
+                    </Aux>
+                );
+            }
+        }
     }
-};
+}
 
 export default ErrorModal;
