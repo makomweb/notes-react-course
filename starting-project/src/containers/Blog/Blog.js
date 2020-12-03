ead6e19cef1c947882a0b3e8343deee5f1996b0b
Martin Komischke <martin.komischke@outlook.com>
2020-12-03 16:43:05 +0100

    turn the absolute path into a relative path

--- starting-project/src/containers/Blog/Blog.js
+++ starting-project/src/containers/Blog/Blog.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
+import { withRouter } from 'react-router';
 
 import './Blog.css';
 import NewPost from './NewPost/NewPost';
@@ -8,6 +9,8 @@ import Posts from './Posts/Posts';
 
 class Blog extends Component {
     render() {
+        const path = this.props.match.url + 'new-post';
+        console.log('path is: ', path);
         return (
             <div className="Blog">
                 <header>
@@ -15,7 +18,7 @@ class Blog extends Component {
                         <ul>
                             <li><Link to="/">Home</Link></li>
                             <li><Link to={{
-                                pathname: "/new-post",
+                                pathname: path,
                                 hash: '#submit',
                                 search: '?quick-submit=true'
                             }}>New Post</Link></li>
@@ -29,4 +32,4 @@ class Blog extends Component {
     }
 }
 
-export default Blog;
+export default withRouter(Blog);
