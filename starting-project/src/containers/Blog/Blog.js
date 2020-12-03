1da022b18a3ecf8d0645f2ca4f8e415f3ee64a00
Martin Komischke <martin.komischke@outlook.com>
2020-12-03 12:52:38 +0100

    extract Posts into its own component

--- starting-project/src/containers/Blog/Blog.js
+++ starting-project/src/containers/Blog/Blog.js
@@ -1,44 +1,11 @@
 import React, { Component } from 'react';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
-import axios from '../../axios.js';
+import Posts from './Posts/Posts';
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null
-    }
-
-    componentDidMount = () => {
-        axios.get('/posts')
-            .then(response => {
-                const posts = response.data.slice(0, 4); // only the first 4 elements are of interest
-                const updatedPosts = posts.map(obj => {
-                    return { ...obj, author: 'Mary' }
-                });
-                //console.log(response);
-                this.setState({ posts: updatedPosts });
-            })
-            .catch(error => {
-                this.setState({ error: true });
-            });
-    }
-
-    onPostClick = (id) => {
-        this.setState({ selectedPostId: id });
-    }
-
     render() {
-        const errorMessage = <p style={{ textAlign: 'center' }}>Something went wrong!</p>
-        const posts = this.state.posts.map(p => {
-            return (
-                <Post key={p.id} title={p.title} author={p.author} clicked={() => this.onPostClick(p.id)} />
-            );
-        });
         return (
             <div className="Blog">
                 <header>
@@ -49,9 +16,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">
-                    {posts}
-                </section>
+                <Posts />
             </div>
         );
     }
---
+++ starting-project/src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import axios from '../../../axios.js';
+import Post from '../../../components/Post/Post.js';
+import './Posts.css';
+
+class Posts extends Component {
+    state = {
+        posts: [],
+        selectedPostId: null,
+        error: false
+    }
+
+    componentDidMount = () => {
+        axios.get('/posts')
+            .then(response => {
+                const posts = response.data.slice(0, 4); // only the first 4 elements are of interest
+                const updatedPosts = posts.map(obj => {
+                    return { ...obj, author: 'Mary' }
+                });
+                //console.log(response);
+                this.setState({ posts: updatedPosts });
+            })
+            .catch(error => {
+                this.setState({ error: true });
+            });
+    }
+
+    onPostClick = (id) => {
+        this.setState({ selectedPostId: id });
+    }
+
+    render() {
+        const errorMessage = <p style={{ textAlign: 'center' }}>Something went wrong!</p>
+        const posts = this.state.posts.map(p => {
+            return (
+                <Post key={p.id} title={p.title} author={p.author} clicked={() => this.onPostClick(p.id)} />
+            );
+        });
+
+        return (
+            <section className="Posts">
+                {this.state.error ? errorMessage : posts}
+            </section>
+        );
+    }
+}
+
+export default Posts;
