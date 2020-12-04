374c774baa73ae95051c84d63ccb88482c56cc7a
Martin Komischke <martin.komischke@outlook.com>
2020-12-04 11:37:24 +0100

    make the nested route work

--- starting-project/src/containers/Blog/Blog.js
+++ starting-project/src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 import { withRouter } from 'react-router';
 
 import './Blog.css';
@@ -32,8 +32,10 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post" component={NewPost} />
+                <Switch>
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/" component={Posts} />
+                </Switch>
             </div>
         );
     }
