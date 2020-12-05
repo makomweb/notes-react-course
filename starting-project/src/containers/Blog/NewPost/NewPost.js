e93f32df327c157ff01ac34454ed26c4f48347b5
Martin Komischke <martin.komischke@outlook.com>
2020-12-05 09:40:21 +0100

    replace does the same as using the 'redirect' component

--- starting-project/src/containers/Blog/NewPost/NewPost.js
+++ starting-project/src/containers/Blog/NewPost/NewPost.js
@@ -26,7 +26,7 @@ class NewPost extends Component {
             .then(response => {
                 console.log(response);
                 //this.setState({ submitted: true });
-                this.props.history.push('/posts');
+                this.props.history.replace('/posts');
             });
     }
 
