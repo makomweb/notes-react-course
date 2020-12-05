7815a0bc328663c92586d65a3edc0942c86b91d2
Martin Komischke <martin.komischke@outlook.com>
2020-12-05 09:36:15 +0100

    redirect conditionally to get back to posts after submission

--- starting-project/src/containers/Blog/NewPost/NewPost.js
+++ starting-project/src/containers/Blog/NewPost/NewPost.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import axios from '../../../axios.js';
 
 import './NewPost.css';
+import { Redirect } from 'react-router-dom';
 
 class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Max'
+        author: 'Max',
+        submitted: false
     }
 
     componentDidMount = () => {
@@ -23,12 +25,17 @@ class NewPost extends Component {
         axios.post('/posts', post)
             .then(response => {
                 console.log(response);
+                this.setState({ submitted: true });
             });
     }
 
     render() {
+        let redirect = this.state.submitted ?
+            <Redirect to="/posts" /> : null;
+
         return (
             <div className="NewPost">
+                {redirect}
                 <h1>Add a Post</h1>
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({ title: event.target.value })} />
