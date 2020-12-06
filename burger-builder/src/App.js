0b1ae517876d0b1dfbd8b615d96c47615c2bc866
Martin Komischke <martin.komischke@outlook.com>
2020-12-06 09:15:39 +0100

    use routing to either show the builder or the checkout component

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import Layout from './HOC/Layout/Layout';
 import BurgerBuilder from './Containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './Containers/Checkout/Checkout.js';
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <Layout>
-        <BurgerBuilder />
-        <Checkout />
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/" exact component={BurgerBuilder} />
       </Layout>
     );
   }
