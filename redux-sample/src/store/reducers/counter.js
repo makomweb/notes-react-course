dd6b62be24de87bd58cc177392c5e088cc5a0df9
Martin Komischke <martin.komischke@outlook.com>
2020-12-07 23:33:57 +0100

    separate the logic of the 2 reducers

---
+++ redux-sample/src/store/reducers/counter.js
@@ -0,0 +1,33 @@
+import * as actions from '../actions';
+
+const initialState = {
+    counter: 0
+}
+
+const reduceCounter = (state = initialState, action) => {
+    switch (action.type) {
+        case (actions.INC):
+            return {
+                ...state,
+                counter: state.counter + 1
+            }
+        case (actions.DEC):
+            return {
+                ...state,
+                counter: state.counter + 1
+            }
+        case (actions.ADD):
+            return {
+                ...state,
+                counter: state.counter + action.value
+            }
+        case (actions.SUB):
+            return {
+                ...state,
+                counter: state.counter - action.value
+            }
+    }
+    return state;
+};
+
+export default reduceCounter;
---
+++ redux-sample/src/store/reducers/result.js
@@ -0,0 +1,28 @@
+import * as actions from '../actions';
+
+const initialState = {
+    results: []
+}
+
+const reduceResults = (state = initialState, action) => {
+    switch (action.type) {
+        case (actions.STORE):
+            return {
+                ...state,
+                results: state.results.concat({
+                    id: new Date(),
+                    value: state.counter
+                })
+            }
+
+        case (actions.DELETE):
+            const updatedArray = state.results.filter(result => result.id !== action.id);
+            return {
+                ...state,
+                results: updatedArray
+            }
+    }
+    return state;
+};
+
+export default reduceResults;
