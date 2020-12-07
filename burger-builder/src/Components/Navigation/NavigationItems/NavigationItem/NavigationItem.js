856571f51017d065ed29c8df61c12acb96f7059e
Martin Komischke <martin.komischke@outlook.com>
2020-12-07 08:07:21 +0100

    pass 'exact' from the outer component

--- burger-builder/src/Components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ burger-builder/src/Components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -5,7 +5,7 @@ import { NavLink } from 'react-router-dom';
 const NavigationItem = (props) => (
     <li className={classes.NavigationItem}>
         <NavLink to={props.link}
-            exact
+            exact={props.exact}
             activeClassName={classes.active}>
             {props.children}
         </NavLink>
