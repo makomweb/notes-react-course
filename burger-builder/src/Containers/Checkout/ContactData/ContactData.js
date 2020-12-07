eae7c42cb7e353facc5e38628fd4c1094f6a12fa
Martin Komischke <martin.komischke@outlook.com>
2020-12-07 17:37:14 +0100

    fix error

--- burger-builder/src/Containers/Checkout/ContactData/ContactData.js
+++ burger-builder/src/Containers/Checkout/ContactData/ContactData.js
@@ -89,6 +89,7 @@ class ContactData extends Component {
                     ]
                 },
                 value: '',
+                validation: {},
                 valid: true
             },
         },
