d8dd85a032791c74f8065e20dc16101d0f4fdbef
Martin Komischke <martin.komischke@outlook.com>
2020-12-07 16:54:44 +0100

    fix flaw with chained validation rules

--- burger-builder/src/Containers/Checkout/ContactData/ContactData.js
+++ burger-builder/src/Containers/Checkout/ContactData/ContactData.js
@@ -128,18 +128,19 @@ class ContactData extends Component {
     }
 
     isValid(value, rules) {
-        let isValid = false;
+        let isValid = true;
         if (rules.required) {
-            isValid = value.trim() !== '';
+            isValid = value.trim() !== '' && isValid;
         }
 
         if (rules.minLength) {
-            isValid = value.length >= rules.minLength;
+            isValid = value.length >= rules.minLength && isValid;
         }
 
         if (rules.maxLength) {
-            isValid = value.length <= rules.maxLength;
+            isValid = value.length <= rules.maxLength && isValid;
         }
+
         return isValid;
     }
 
