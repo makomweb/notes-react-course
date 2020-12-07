ae5ee5071ccded9cc7186f7521f359294b599748
Martin Komischke <martin.komischke@outlook.com>
2020-12-07 07:51:35 +0100

    start adding Order and Orders components

---
+++ burger-builder/src/Containers/Orders/Orders.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Orders extends Component {
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Orders;
