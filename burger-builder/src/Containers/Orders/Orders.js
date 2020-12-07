b12f1209a11027ffa811b35e625a481aa966a8d9
Martin Komischke <martin.komischke@outlook.com>
2020-12-07 08:15:04 +0100

    fetch orders and store them in the state of the Orders component

--- burger-builder/src/Containers/Orders/Orders.js
+++ burger-builder/src/Containers/Orders/Orders.js
@@ -1,7 +1,30 @@
 import React, { Component } from 'react';
 import Order from '../../Components/Order/Order.js';
+import AxiosInstance from '../../AxiosInstance.js';
 
 class Orders extends Component {
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount = () => {
+        AxiosInstance.get('/orders.json')
+            .then(resp => {
+                const fetchedOrders = [];
+                for (let key in resp.data) {
+                    fetchedOrders.push({
+                        ...resp.data[key],
+                        id: key
+                    });
+                }
+
+                this.setState({ loading: false, orders: fetchedOrders });
+            })
+            .catch(err => {
+                this.setState({ loading: false });
+            });
+    }
 
     render() {
         return (
