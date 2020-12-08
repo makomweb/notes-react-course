085dada0b660826b67668d94cfcb35617fb273e7
Martin Komischke <martin.komischke@outlook.com>
2020-12-08 22:29:09 +0100

    use convenience methods

--- redux-sample/src/store/reducers/result.js
+++ redux-sample/src/store/reducers/result.js
@@ -5,26 +5,25 @@ const initialState = {
     results: []
 }
 
+const deleteResult = (state, action) => {
+    const updatedArray = state.results.filter(result => result.id !== action.id);
+    return updateObject(state, { results: updatedArray });
+}
+
+const storeResult = (state, action) => {
+    const updatedArray = state.results.concat({
+        id: new Date(),
+        value: action.result
+    });
+    return updateObject(state, {
+        results: updatedArray
+    });
+}
+
 const reduceResults = (state = initialState, action) => {
     switch (action.type) {
-        case (actions.STORE):
-            {
-                const updatedArray = state.results.concat({
-                    id: new Date(),
-                    value: action.result
-                });
-                return updateObject(state, {
-                    results: updatedArray
-                });
-            }
-
-        case (actions.REMOVE):
-            {
-                const updatedArray = state.results.filter(result => result.id !== action.id);
-                return updateObject(state, {
-                    results: updatedArray
-                });
-            }
+        case (actions.STORE): return storeResult(state, action);
+        case (actions.REMOVE): return deleteResult(state, action);
     }
     return state;
 };
