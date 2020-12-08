bc496d5969bd76b9fb5494ce53a3eb242d16f618
Martin Komischke <martin.komischke@outlook.com>
2020-12-08 11:26:20 +0100

    add Redux provider and store to the app

--- burger-builder/src/index.js
+++ burger-builder/src/index.js
@@ -5,11 +5,19 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import Reducer from './Store/reducer';
+
+const store = createStore(Reducer);
 
 const MyApp = (
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 );
 
 ReactDOM.render(MyApp, document.getElementById('root'));
