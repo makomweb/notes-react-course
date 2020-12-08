8a262de987feb178c41de69d75428a6b46aa94ae
Martin Komischke <martin.komischke@outlook.com>
2020-12-08 18:42:58 +0100

    enable thunk middleware

--- redux-sample/src/index.js
+++ redux-sample/src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import reduceCounter from './store/reducers/counter';
 import reduceResults from './store/reducers/result';
-import counterControl from './components/CounterControl/CounterControl';
 
 const rootReducer = combineReducers({
     counter: reduceCounter,
@@ -30,7 +30,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
     rootReducer,
-    composeEnhancers(applyMiddleware(logger))
+    composeEnhancers(applyMiddleware(logger, thunk))
 );
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
