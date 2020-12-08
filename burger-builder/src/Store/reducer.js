f00e2891acab0619328c0418ed30fcfa02f10584
Martin Komischke <martin.komischke@outlook.com>
2020-12-08 11:34:32 +0100

    deep copy the current state (to immutably change the new value)

--- burger-builder/src/Store/reducer.js
+++ burger-builder/src/Store/reducer.js
@@ -1,12 +1,34 @@
 import * as actions from './actions';
 
 const initialState = {
-    ingredients: null,
+    ingredients: {
+        lettuce: 0,
+        bacon: 0,
+        cheese: 0,
+        beef: 0
+    },
     totalPrice: 4
 }
 
 const reducer = (state = initialState, action) => {
-
+    switch (action.type) {
+        case actions.ADD_INGREDIENT:
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients
+                }
+            };
+        case actions.ADD_INGREDIENT:
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients
+                }
+            };
+        default:
+            return state;
+    }
 }
 
 export default reducer;
