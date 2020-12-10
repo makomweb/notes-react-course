218c94693f4b7da41fd164f83b565cb33a14a7c1
Martin Komischke <martin.komischke@outlook.com>
2020-12-10 13:14:31 +0100

    implement the order reducer

---
+++ burger-builder/src/Store/Reducers/Order.js
@@ -0,0 +1,34 @@
+import * as actionTypes from '../Actions/actionTypes';
+
+const initialState = {
+    orders: [],
+    loading: false
+}
+
+const reducer = (state = initialState, action) => {
+    const { type } = action;
+    switch (type) {
+
+        case actionTypes.PURCHASE_BURGER_SUCCESS: {
+            const order = {
+                ...action.orderData,
+                id: action.orderId
+            }
+            return {
+                ...state,
+                loading: false,
+                orders: this.state.orders.concat(order)
+            }
+        }
+
+        case actionTypes.PURCHASE_BURGER_FAIL:
+            return {
+                ...state,
+                loading: false
+            }
+
+        default: return state;
+    }
+}
+
+export default reducer;
