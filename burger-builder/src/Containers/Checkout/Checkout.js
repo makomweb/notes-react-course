2c8b78200af15313ecfe3be3cf0e9498568517b6
Martin Komischke <martin.komischke@outlook.com>
2020-12-10 13:32:40 +0100

    redirect to the root if the ingredients are not there yet

--- burger-builder/src/Containers/Checkout/Checkout.js
+++ burger-builder/src/Containers/Checkout/Checkout.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import CheckoutSummary from '../../Components/Order/CheckoutSummary/CheckoutSummary.js';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import ContactData from './ContactData/ContactData.js';
 import { connect } from 'react-redux';
 
@@ -14,17 +14,40 @@ class Checkout extends Component {
         this.props.history.replace('/checkout/contact-data');
     }
 
+    canCheckout() {
+        const { ingr } = this.props;
+
+        if (ingr) {
+            const sum = Object.keys(ingr)
+                .map(key => {
+                    return ingr[key];
+                })
+                .reduce((acc, element) => {
+                    return acc + element;
+                }, 0);
+
+            return sum > 0;
+        }
+
+        return false;
+    }
+
     render() {
-        return (
-            <div>
-                <CheckoutSummary
-                    ingredients={this.props.ingr}
-                    cancel={this.onCheckoutCancelled}
-                    continue={this.onCheckoutContinue} />
-                <Route path={this.props.match.path + '/contact-data'}
-                    component={ContactData} />
-            </div>
-        );
+        let summary = <Redirect to="/" />
+        if (this.canCheckout()) {
+            summary = (
+                <div>
+                    <CheckoutSummary
+                        ingredients={this.props.ingr}
+                        cancel={this.onCheckoutCancelled}
+                        continue={this.onCheckoutContinue} />
+                    <Route path={this.props.match.path + '/contact-data'}
+                        component={ContactData} />
+                </div>
+            );
+        }
+
+        return summary;
     }
 }
 
