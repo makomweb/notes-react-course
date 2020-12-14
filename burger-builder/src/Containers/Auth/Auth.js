f467bc6ef828343bca60fea9459183a001adcd7e
Martin Komischke <martin.komischke@outlook.com>
2020-12-14 12:16:57 +0100

    implement a form with a button

--- burger-builder/src/Containers/Auth/Auth.js
+++ burger-builder/src/Containers/Auth/Auth.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Input from '../../Components/UI/Input/Input';
+import Button from '../../Components/UI/Button/Button';
 
 class Auth extends Component {
 
@@ -12,7 +14,8 @@ class Auth extends Component {
                 },
                 validation: {
                     required: true,
-                    isEmail: true
+                    isEmail: true,
+                    errormessage: 'Please enter a valid email address!'
                 },
                 valid: false,
                 touched: false,
@@ -27,7 +30,8 @@ class Auth extends Component {
                 },
                 validation: {
                     required: true,
-                    minLength: 6
+                    minLength: 6,
+                    errormessage: 'Your password must be at least 6 characters long!'
                 },
                 valid: false,
                 touched: false,
@@ -37,10 +41,39 @@ class Auth extends Component {
     }
 
     render() {
+        const formElements = [];
+        for (let key in this.state.orderForm) {
+            formElements.push({
+                id: key,
+                data: this.state.orderForm[key],
+            });
+        }
+
+        const form = formElements.map(elem => (
+            <Input
+                key={elem.id}
+                type={elem.data.type}
+                config={elem.data.config}
+                value={elem.data.value}
+                invalid={!elem.data.valid}
+                errormessage={elem.data.validation ? elem.data.validation.errormessage : null}
+                shouldValidate={elem.data.validation}
+                touched={elem.data.touched}
+                changed={(event) => this.onInputChanged(event, elem.id)} />
+        ));
+
+        // if (this.props.loading) {
+        //     form = <Spinner />
+        // }
+
         return (
             <div>
-                <form>
-
+                <form onSubmit={this.onOrderTapped}>
+                    {form}
+                    {/* <Button buttonType="Success"
+                        disabled={!this.state.formIsValid}
+                        clicked={this.onOrderTapped}>SUBMIT</Button> */}
+                    <Button buttonType="Success">SUBMIT</Button>
                 </form>
             </div>
         );
