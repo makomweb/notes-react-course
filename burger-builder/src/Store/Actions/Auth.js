8ea2d3fe1c1c666758bde8333c20d60f67871032
Martin Komischke <martin.komischke@outlook.com>
2020-12-14 14:43:17 +0100

    add login REST call to firebase
    https://console.firebase.google.com/u/0/project/emkay-burger-builder/settings/general
    
    Find the API key under Settings > General > Web-API-Key.

--- burger-builder/src/Store/Actions/Auth.js
+++ burger-builder/src/Store/Actions/Auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../Actions/actionTypes';
+import axios from 'axios';
 
 const authStart = () => {
     return {
@@ -23,6 +24,21 @@ const authFailed = (error) => {
 export const authenticate = (email, password) => {
     return dispatch => {
         dispatch(authStart());
-
+        const apiKey = 'AIzaSyDA02EJ4cRq6kXJeEAcX2EbJXi56-Fqt1A';
+        const address = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`;
+        const payload = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }
+        axios.post(address, payload)
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response.data));
+            })
+            .catch(error => {
+                console.log(error);
+                dispatch(authFailed(error));
+            });
     }
 }
