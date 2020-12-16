b503ea3327a360930d1c4b7019467c8ddd8b0d85
Martin Komischke <martin.komischke@outlook.com>
2020-12-16 12:00:40 +0100

    hide 'orders' when not authenticated

--- burger-builder/src/Components/Navigation/NavigationItems/NavigationItems.js
+++ burger-builder/src/Components/Navigation/NavigationItems/NavigationItems.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import classes from './NavigationItems.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
@@ -6,9 +6,12 @@ const NavigationItems = props => {
     return (
         <ul className={classes.NavigationItems}>
             <NavigationItem link="/" exact >Burger Builder</NavigationItem>
-            <NavigationItem link="/orders" >Orders</NavigationItem>
+
             {props.isAuthenticated ?
-                <NavigationItem link="/logout" >Logout</NavigationItem> :
+                (<Fragment>
+                    <NavigationItem link="/orders" >Orders</NavigationItem>
+                    <NavigationItem link="/logout" >Logout</NavigationItem>
+                </Fragment>) :
                 <NavigationItem link="/auth" >Authenticate</NavigationItem>}
         </ul>);
 }
