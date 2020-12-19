d5e12615292f7c19c75361dd15f857500983e919
Martin Komischke <martin.komischke@outlook.com>
2020-12-19 17:10:04 +0100

    add test for the BurgerBuilder component

---
+++ burger-builder/src/Containers/BurgerBuilder/BurgerBuilder.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { BurgerBuilder } from './BurgerBuilder';
+import BuildControls from '../../Components/Burger/BuildControls/BuildControls';
+
+configure({ adapter: new Adapter() });
+describe(
+    '<BurgerBuilder />',
+    () => {
+        let wrapper;
+
+        beforeEach(() => {
+            wrapper = shallow(<BurgerBuilder />);
+        });
+
+        it('should render <BuildControls /> when receiving ingredients!',
+            () => {
+                wrapper.setProps({ ingredients: { lettuce: 0 } });
+                expect(wrapper.find(BuildControls)).toHaveLength(1);
+            });
+
+    }
+);
