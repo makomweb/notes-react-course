d2bcf556eaa586e54041376389e83f2f4ca96335
Martin Komischke <martin.komischke@outlook.com>
2020-12-21 15:51:34 +0100

    use getInitialProps to prepopulate props asynchronously

--- next-js-sample/pages/index.js
+++ next-js-sample/pages/index.js
@@ -3,10 +3,21 @@ import Link from 'next/link';
 import Router from 'next/router';
 
 class IndexPage extends Component {
+    static async getInitialProps(context) {
+        console.log(context);
+        const promise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve({ appName: 'Super App' });
+            }, 1000);
+        });
+
+        return promise;
+    }
+
     render() {
         return (
             <div>
-                <h1>The main page</h1>
+                <h1>The main page of {this.props.appName}</h1>
                 <p>Go to <Link href="/auth"><a>Auth</a></Link></p>
                 <button onClick={() => Router.push('/auth')}>Auth</button>
             </div>
