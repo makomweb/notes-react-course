5672c946f836957639803d45ceeac44a0af792e2
Martin Komischke <martin.komischke@outlook.com>
2020-12-21 15:24:12 +0100

    add a button

--- next-js-sample/pages/index.js
+++ next-js-sample/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Link from 'next/link';
+import Router from 'next/router';
 
 const indexPage = () => {
     return (
         <div>
             <h1>The main page</h1>
             <p>Go to <Link href="/auth"><a>Auth</a></Link></p>
+            <button onClick={() => Router.push('/auth')}>Auth</button>
         </div>
     );
 }
