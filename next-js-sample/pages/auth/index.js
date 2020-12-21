e531de8a83ecafea3e683b33aaca4bc5387798ef
Martin Komischke <martin.komischke@outlook.com>
2020-12-21 15:27:17 +0100

    add + render a component

--- next-js-sample/pages/auth/index.js
+++ next-js-sample/pages/auth/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import User from '../../components/User';
 
 const authIndexPage = () => {
     return (
         <div>
             <h1>The auth page</h1>
+            <User name="Paul" age={22} />
         </div>
     );
 }
