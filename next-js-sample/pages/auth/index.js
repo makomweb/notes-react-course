3b886991516815f41450711f2d3e58a90c2bf8a1
Martin Komischke <martin.komischke@outlook.com>
2020-12-21 15:55:08 +0100

    pass props to the functional component (index)

--- next-js-sample/pages/auth/index.js
+++ next-js-sample/pages/auth/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import User from '../../components/User';
 
-const authIndexPage = () => {
+const authIndexPage = (props) => {
     return (
         <div>
-            <h1>The auth page</h1>
+            <h1>The auth page - {props.appName}</h1>
             <User name="Paul" age={22} />
         </div>
     );
+}
+
+authIndexPage.getInitialProps = (context) => {
+    console.log(context);
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ appName: 'Super App (Auth)' });
+        }, 1000);
+    });
+
+    return promise;
 }
 
 export default authIndexPage;
