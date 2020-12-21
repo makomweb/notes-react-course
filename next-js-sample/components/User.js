b7d91e7e1bdc3881f846064d86b13abfa50a8b68
Martin Komischke <martin.komischke@outlook.com>
2020-12-21 15:40:49 +0100

    style the user component

--- next-js-sample/components/User.js
+++ next-js-sample/components/User.js
@@ -5,6 +5,14 @@ const User = props => {
         <div>
             <h1>{props.name}</h1>
             <p>Age: {props.age}</p>
+            <style jsx>{`
+                div {
+                    border: 1px solid #eee;
+                    border-shadow: 0 2p 3px #ccc
+                    padding: 20px;
+                    text-align: center;
+                }
+            `}</style>
         </div>
     );
 }
