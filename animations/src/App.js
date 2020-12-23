c118e7162598932eaae03d23667eb4a63f515dbe
Martin Komischke <martin.komischke@outlook.com>
2020-12-23 11:45:42 +0100

    render a paragraph inside the transition

--- animations/src/App.js
+++ animations/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import Transition from 'react-transition-group/Transition';
 import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
@@ -29,13 +29,9 @@ class App extends Component {
         <h1>React Animations</h1>
         <button className="Button" onClick={this.handleToggle}>Toggle</button>
         <br></br>
-        {this.state.showBlock ?
-          <div style={{
-            backgroundColor: 'red',
-            width: 100,
-            height: 100,
-            margin: 'auto'
-          }}></div> : null}
+        <Transition in={this.state.showBlock} duration={1000} >
+          {state => <p>{state}</p>}
+        </Transition>
         {this.state.modalIsOpen ? <Modal closed={this.closeModal} show /> : null}
         {this.state.modalIsOpen ? <Backdrop show /> : null}
         <button className="Button" onClick={this.showModal}>Open Modal</button>
