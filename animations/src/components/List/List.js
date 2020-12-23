bdeff35c1cb4c8aa28f94cb8113893061ce6857f
Martin Komischke <martin.komischke@outlook.com>
2020-12-23 16:54:17 +0100

    use the TransitionGroup container for the unordered list

--- animations/src/components/List/List.js
+++ animations/src/components/List/List.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Transition from 'react-transition-group/Transition';
+import TransitionGroup from 'react-transition-group/TransitionGroup';
 
 import './List.css';
 
@@ -23,21 +25,23 @@ class List extends Component {
         });
     }
 
-    render () {
-        const listItems = this.state.items.map( (item, index) => (
-            <li 
+    render() {
+        const listItems = this.state.items.map((item, index) => (
+            <li
                 key={index}
-                className="ListItem" 
+                className="ListItem"
                 onClick={() => this.removeItemHandler(index)}>{item}</li>
-        ) );
+        ));
 
         return (
             <div>
                 <button className="Button" onClick={this.addItemHandler}>Add Item</button>
                 <p>Click Item to Remove.</p>
-                <ul className="List">
+                <TransitionGroup
+                    component="ul"
+                    className="List">
                     {listItems}
-                </ul>
+                </TransitionGroup>
             </div>
         );
     }
