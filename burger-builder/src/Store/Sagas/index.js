72ce046a0f30481eaa4cfaf53e99e48af274f712
Martin Komischke <martin.komischke@outlook.com>
2021-01-02 16:28:19 +0100

    swap saga listeners

--- burger-builder/src/Store/Sagas/index.js
+++ burger-builder/src/Store/Sagas/index.js
@@ -3,6 +3,6 @@ import * as actionTypes from '../Actions/actionTypes';
 import { logoutSaga, checkoutAuthTimeoutSaga } from './Auth'
 
 export function* watchAll() {
-    yield takeEvery(actionTypes.AUTH_INIT_LOGOUT, logoutSaga);
     yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkoutAuthTimeoutSaga);
+    yield takeEvery(actionTypes.AUTH_INIT_LOGOUT, logoutSaga);
 }
