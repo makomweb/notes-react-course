ef9b65634bcbb4f63c04dd9d867c6590f02636da
Martin Komischke <martin.komischke@outlook.com>
2021-01-02 16:45:41 +0100

    add saga listener

--- burger-builder/src/Store/Sagas/index.js
+++ burger-builder/src/Store/Sagas/index.js
@@ -1,8 +1,9 @@
 import { takeEvery } from 'redux-saga/effects';
 import * as actionTypes from '../Actions/actionTypes';
-import { logoutSaga, checkoutAuthTimeoutSaga } from './Auth'
+import { logoutSaga, checkoutAuthTimeoutSaga, authUserSaga } from './Auth'
 
 export function* watchAll() {
     yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkoutAuthTimeoutSaga);
     yield takeEvery(actionTypes.AUTH_INIT_LOGOUT, logoutSaga);
+    yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
 }
