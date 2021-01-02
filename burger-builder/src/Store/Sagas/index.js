6c9a07e3a044d32bbc8607a1280fabf5b8c9f05e
Martin Komischke <martin.komischke@outlook.com>
2021-01-02 19:54:55 +0100

    enable simultaneous execution of the saga handlers

--- burger-builder/src/Store/Sagas/index.js
+++ burger-builder/src/Store/Sagas/index.js
@@ -1,14 +1,16 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, all } from 'redux-saga/effects';
 import * as actionTypes from '../Actions/actionTypes';
 import { logoutSaga, checkoutAuthTimeoutSaga, authUserSaga, authCheckStateSaga } from './Auth'
 import { fetchPricesSaga } from './BurgerBuilder';
 import { purchaseBurgerSaga, fetchOrdersSaga } from './Orders';
 
 export function* watchAll() {
-    yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkoutAuthTimeoutSaga);
-    yield takeEvery(actionTypes.AUTH_INIT_LOGOUT, logoutSaga);
-    yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
-    yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga);
+    yield all([
+        takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkoutAuthTimeoutSaga),
+        takeEvery(actionTypes.AUTH_INIT_LOGOUT, logoutSaga),
+        takeEvery(actionTypes.AUTH_USER, authUserSaga),
+        takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga)
+    ]);
 }
 
 export function* watchBurgerBuilder() {
