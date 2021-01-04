c8e9bb6ac92d5acd355740023921715815095857
Martin Komischke <martin.komischke@outlook.com>
2021-01-04 11:48:17 +0100

    fix warning (developer console)

--- react-hooks/src/components/Ingredients/IngredientForm.js
+++ react-hooks/src/components/Ingredients/IngredientForm.js
@@ -19,13 +19,13 @@ const IngredientForm = React.memo(props => {
             <label htmlFor="title">Name</label>
             <input type="text" id="title"
               value={inputState[0].title}
-              onChange={event => inputState[1]({ title: event.target.value })} />
+              onChange={event => inputState[1]({ title: event.target.value, amount: inputState[0].amount })} />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
             <input type="number" id="amount"
               value={inputState[0].amount}
-              onChange={event => inputState[1]({ amount: event.target.value })} />
+              onChange={event => inputState[1]({ amount: event.target.value, title: inputState[0].title })} />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
