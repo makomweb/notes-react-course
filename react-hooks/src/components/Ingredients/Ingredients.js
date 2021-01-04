5962e23c52280d40b5034ce39b9a10c648d3b87a
Martin Komischke <martin.komischke@outlook.com>
2021-01-04 16:20:55 +0100

    add IngredientList component and connect it with the state

--- react-hooks/src/components/Ingredients/Ingredients.js
+++ react-hooks/src/components/Ingredients/Ingredients.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
 function Ingredients() {
+  const [ingredients, setIngredients] = useState([]);
+
   return (
     <div className="App">
       <IngredientForm />
-
       <section>
         <Search />
         {/* Need to add list here! */}
+        <IngredientList ingredients={ingredients} />
       </section>
     </div>
   );
