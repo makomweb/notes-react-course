c38995c7ce468a3cbb4c6f8cdeb5cea0f189241d
Martin Komischke <martin.komischke@outlook.com>
2021-01-04 12:03:10 +0100

    prevent runtime error (closure where event is reused!)

--- react-hooks/src/components/Ingredients/IngredientForm.js
+++ react-hooks/src/components/Ingredients/IngredientForm.js
@@ -19,23 +19,27 @@ const IngredientForm = React.memo(props => {
             <label htmlFor="title">Name</label>
             <input type="text" id="title"
               value={inputState[0].title}
-              onChange={event =>
+              onChange={event => {
+                const newTitle = event.target.value;
                 inputState[1](prevInputState =>
                 ({
-                  title: event.target.value,
+                  title: newTitle,
                   amount: prevInputState.amount
-                }))} />
+                }))
+              }} />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
             <input type="number" id="amount"
               value={inputState[0].amount}
-              onChange={event =>
+              onChange={event => {
+                const newAmount = event.target.value;
                 inputState[1](prevInputState =>
                 ({
-                  amount: event.target.value,
+                  amount: newAmount,
                   title: prevInputState.title
-                }))} />
+                }))
+              }} />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
