56f2c7e05fd73959ae1732346868bb408f965f90
Martin Komischke <martin.komischke@outlook.com>
2021-01-06 09:59:20 +0100

    use the context to login

--- react-hooks/src/components/Auth.js
+++ react-hooks/src/components/Auth.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import Card from './UI/Card';
 import './Auth.css';
+import { AuthContext } from '../context/auth-context';
 
 const Auth = props => {
-  const loginHandler = () => {};
+  const context = useContext(AuthContext);
+  const loginHandler = () => {
+    context.login();
+  };
 
   return (
     <div className="auth">
