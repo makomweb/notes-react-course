6c6917a779e34a620da25f11eb83ddbe496ea97d
Martin Komischke <martin.komischke@outlook.com>
2021-01-06 17:38:55 +0100

    merge reducer logic

--- react-hooks/src/hooks/http.js
+++ react-hooks/src/hooks/http.js
@@ -1,34 +1,26 @@
 import { useCallback, useReducer } from 'react';
 
-const createReducer = (state, action) => {
+const reducer = (state, action) => {
     const { type } = action;
     switch (type) {
-        case 'REQUEST': return { loading: true }
-        case 'FINISHED': return { loading: false, ingredient: action.ingredient }
-        case 'FAILED': return { loading: false, error: action.error }
-        default: throw new Error('Should not get here!');
-    }
-}
-
-const removeReducer = (state, action) => {
-    const { type } = action;
-    switch (type) {
-        case 'REQUEST': return { loading: true }
-        case 'FINISHED': return { loading: false, id: action.id }
+        case 'CREATION_ISSUED':
+        case 'REMOVAL_ISSUED': return { loading: true }
+        case 'CREATION_FINISHED': return { loading: false, ingredient: action.ingredient }
+        case 'REMOVAL_FINISHED': return { loading: false, id: action.id }
         case 'FAILED': return { loading: false, error: action.error }
         default: throw new Error('Should not get here!');
     }
 }
 
 export const useCreateIngredient = () => {
-    const [state, reduce] = useReducer(createReducer, {
+    const [state, reduce] = useReducer(reducer, {
         loading: false,
         ingredient: null,
         error: null,
     });
 
     const sendRequest = useCallback(ingredient => {
-        reduce({ type: 'REQUEST' });
+        reduce({ type: 'CREATION_ISSUED' });
         fetch('https://react-hooks-update-29adc-default-rtdb.firebaseio.com/ingredients.json',
             {
                 method: 'POST',
@@ -41,7 +33,7 @@ export const useCreateIngredient = () => {
                 return response.json();
             })
             .then(data => {
-                reduce({ type: 'FINISHED', ingredient: { id: data.name, ...ingredient } });
+                reduce({ type: 'CREATION_FINISHED', ingredient: { id: data.name, ...ingredient } });
             })
             .catch(error => {
                 reduce({ type: 'FAILED', error: 'Request has failed!' });
@@ -55,20 +47,20 @@ export const useCreateIngredient = () => {
 }
 
 export const useRemoveIngredient = () => {
-    const [state, reduce] = useReducer(removeReducer, {
+    const [state, reduce] = useReducer(reducer, {
         loading: false,
         id: null,
         error: null,
     });
 
     const sendRequest = useCallback(id => {
-        reduce({ type: 'REQUEST' });
+        reduce({ type: 'REMOVAL_ISSUED' });
         fetch(`https://react-hooks-update-29adc-default-rtdb.firebaseio.com/ingredients/${id}.json`,
             {
                 method: 'DELETE',
             })
             .then(response => {
-                reduce({ type: 'FINISHED', id: id });
+                reduce({ type: 'REMOVAL_FINISHED', id: id });
             })
             .catch(error => {
                 reduce({ type: 'FAILED', error: 'Request has failed!' });
