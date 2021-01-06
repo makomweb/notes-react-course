4c9b331963a5559e16a5f40799371c71f772570f
Martin Komischke <martin.komischke@outlook.com>
2021-01-06 09:30:28 +0100

    replace useState() with useReducer()

--- react-hooks/src/components/Ingredients/Ingredients.js
+++ react-hooks/src/components/Ingredients/Ingredients.js
@@ -1,29 +1,44 @@
-import React, { useState, useReducer, useCallback } from 'react';
+import React, { useReducer, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import Search from './Search';
 import ErrorModal from '../UI/ErrorModal';
 
-const ingredientReducer = (ingredients, action) => {
+const ingredientReducer = (state, action) => {
   const { type } = action;
   switch (type) {
     case 'SET': return action.ingredients;
-    case 'ADD': return [...ingredients, action.ingredient];
-    case 'REMOVE': return ingredients.filter(i => i.id !== action.id);
+    case 'ADD': return [...state, action.ingredient];
+    case 'REMOVE': return state.filter(i => i.id !== action.id);
     default:
       throw new Error('Should not get here!');
   }
 }
 
+const httpReducer = (state, action) => {
+  const { type } = action;
+  switch (type) {
+    case 'REQUEST': return { loading: true, error: null }
+    case 'FINISHED': return { ...state, loading: false }
+    case 'FAILED': return { loading: false, error: action.error }
+    case 'CLEAR': return { ...state, error: null }
+    default: throw new Error('Should not get here!');
+  }
+}
+
 function Ingredients() {
   //const [userIngredients, setUserIngredients] = useState([]);
-  const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
+  const [userIngredients, reduceIngredients] = useReducer(ingredientReducer, []);
+  /*
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
+  */
+  const [httpState, reduceHttpState] = useReducer(httpReducer, { loading: false, error: null });
 
   const onIngredientAdded = ingredient => {
-    setIsLoading(true);
+    //setIsLoading(true);
+    reduceHttpState({ type: 'REQUEST' });
 
     fetch('https://react-hooks-update-29adc-default-rtdb.firebaseio.com/ingredients.json',
       {
@@ -41,7 +56,7 @@ function Ingredients() {
           ...ingredient
         }]);
         */
-        dispatch({
+        reduceIngredients({
           type: 'ADD', ingredient: {
             id: responseData.name,
             ...ingredient
@@ -49,13 +64,17 @@ function Ingredients() {
         });
       })
       .catch(error => {
-        setError('Adding ingredient has failed! ', error);
+        //setError('Adding ingredient has failed! ', error);
+        reduceHttpState({ type: 'FAILED', error: 'Adding ingredient has failed!' });
       })
-      .finally(() => setIsLoading(false));
+      .finally(() => {
+        reduceHttpState({ type: 'FINISHED' });
+      });
   }
 
   const onIngredientRemoved = id => {
-    setIsLoading(true);
+    //setIsLoading(true);
+    reduceHttpState({ type: 'REQUEST' });
 
     fetch(`https://react-hooks-update-29adc-default-rtdb.firebaseio.com/ingredients/${id}.json`,
       {
@@ -66,30 +85,37 @@ function Ingredients() {
         const newIngredients = userIngredients.filter(obj => obj.id !== id);
         setUserIngredients(newIngredients);
         */
-        dispatch({ type: 'REMOVE', id: id });
+        reduceIngredients({ type: 'REMOVE', id: id });
       })
       .catch(error => {
-        setError('Removing ingredient has failed! ', error);
+        //setError('Removing ingredient has failed! ', error);
+        reduceHttpState({ type: 'FAILED', error: 'Removing ingredient has failed!' });
       })
-      .finally(() => setIsLoading(false));
+      .finally(() => {
+        reduceHttpState({ type: 'FINISHED' });
+      });
   }
 
   const onLoadIngredients = useCallback(ingredients => {
     //setUserIngredients(ingredients);
-    dispatch({ type: 'SET', ingredients: ingredients });
+    reduceIngredients({ type: 'SET', ingredients: ingredients });
   }, []);
 
   const onErrorClose = () => {
+    /*
     setError(null);
     setIsLoading(false);
+    */
+    reduceHttpState({ type: 'CLEAR' });
 
     // Note: both updates are batched together which causes a single render cycle of the component!
   }
 
+  //const { error, loading } = httpState;
   return (
     <div className="App">
-      {error ? <ErrorModal onClose={onErrorClose}>{error}</ErrorModal> : null}
-      <IngredientForm addIngredient={onIngredientAdded} isLoading={isLoading} />
+      {httpState.error ? <ErrorModal onClose={onErrorClose}>{httpState.error}</ErrorModal> : null}
+      <IngredientForm addIngredient={onIngredientAdded} isLoading={httpState.loading} />
       <section>
         <Search loadIngredients={onLoadIngredients} />
         <IngredientList ingredients={userIngredients} onRemoveItem={onIngredientRemoved} />
