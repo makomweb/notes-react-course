0bd2df5333b599a77ac6fa70515f633ac7fb103a
Martin Komischke <martin.komischke@outlook.com>
2021-01-06 09:56:51 +0100

    use the context to decide whether we are logged in or not

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import Ingredients from './components/Ingredients/Ingredients';
+import Auth from './components/Auth';
+import { AuthContext } from './context/auth-context';
 
 const App = props => {
-  return <Ingredients />;
+  const context = useContext(AuthContext);
+  return context.isAuth ? <Ingredients /> : <Auth />;
 };
 
 export default App;
