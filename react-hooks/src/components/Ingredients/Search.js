bac86a5be9ce9907d51306b9532db9883bbdcd56
Martin Komischke <martin.komischke@outlook.com>
2021-01-07 10:27:36 +0100

    add loading indicator and error handling for search

--- react-hooks/src/components/Ingredients/Search.js
+++ react-hooks/src/components/Ingredients/Search.js
@@ -1,41 +1,42 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { useFetchIngredients } from '../../hooks/http';
 
 import Card from '../UI/Card';
+import ErrorModal from '../UI/ErrorModal';
 import './Search.css';
 
 const Search = React.memo(props => {
-  const { loadIngredients } = props;
+  const { provideIngredients } = props;
   const [enteredFilter, setEnteredFilter] = useState('');
   const inputRef = useRef();
-  const fetch = useFetchIngredients();
-
-  /*
-  useEffect(() => {
-    loadIngredients(fetch.ingredients);
-  }, [fetch.ingredients]);
+  const { issue, loading, error, ingredients, clear } = useFetchIngredients();
 
   useEffect(() => {
-    fetch.sendRequest('');
-  }, []);
-  */
-  /*
+    if (ingredients) {
+      provideIngredients(ingredients);
+    }
+  }, [ingredients, provideIngredients]);
+
   useEffect(() => {
     const timer = setTimeout(() => {
       // if previous value is the same like current value (note: it changes when typing!)
-      if (enteredFilter === inputRef.current.value) {      
-        fetch.sendRequest(enteredFilter);
+      if (enteredFilter === inputRef.current.value) {
+        issue(enteredFilter);
       }
     }, 500);
 
     return () => {
       clearTimeout(timer);
     };
-  }, [enteredFilter, loadIngredients, inputRef, fetch]);
-  */
+  }, [enteredFilter, provideIngredients, inputRef, issue]);
+
+  const clearError = useCallback(() => {
+    clear();
+  }, [clear]);
 
   return (
     <section className="search">
+      {error ? <ErrorModal onClose={clearError}>{error}</ErrorModal> : null}
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
@@ -43,6 +44,7 @@ const Search = React.memo(props => {
             ref={inputRef}
             value={enteredFilter}
             onChange={event => setEnteredFilter(event.target.value)} />
+          {loading ? <span>loading...</span> : null}
         </div>
       </Card>
     </section>
