73f8887177eb71259c820f3b701b98bf538aa285
Martin Komischke <martin.komischke@outlook.com>
2021-01-08 14:44:51 +0100

    turn Layout into a functional component

--- burger-builder/src/HOC/Layout/Layout.js
+++ burger-builder/src/HOC/Layout/Layout.js
@@ -1,35 +1,31 @@
-import React, { Component } from 'react';
+import React, { useCallback, useState } from 'react';
 import Auxiliary from '../Auxiliary/Auxiliary';
 import SideDrawer from '../../Components/Navigation/SideDrawer/SideDrawer';
 import Toolbar from '../../Components/Navigation/Toolbar/Toolbar.js';
 import classes from './Layout.css';
 import { connect } from 'react-redux';
 
-class Layout extends Component {
-    state = {
-        sideDrawerVisible: false
-    }
+const Layout = props => {
+    const [sideDrawerVisible, setSideDrawerVisible] = useState(false);
 
-    onSideDrawerToggle = () => {
-        this.setState((prevState) => {
-            this.setState({ sideDrawerVisible: !prevState.sideDrawerVisible });
-        });
-    }
+    const onSideDrawerToggle = useCallback(() => {
+        setSideDrawerVisible(prevState => !prevState);
+    }, []);
 
-    render() {
-        return (
-            <Auxiliary>
-                <Toolbar
-                    toggleDrawer={this.onSideDrawerToggle}
-                    isAuthenticated={this.props.isAuthenticated} />
-                <SideDrawer open={this.state.sideDrawerVisible} closed={this.onSideDrawerToggle} />
-                <main className={classes.Content}>
-                    {this.props.children}
-                </main>
-            </Auxiliary>
-        );
-    }
+    return (
+        <Auxiliary>
+            <Toolbar
+                toggleDrawer={onSideDrawerToggle}
+                isAuthenticated={props.isAuthenticated} />
+            <SideDrawer open={sideDrawerVisible} closed={onSideDrawerToggle} />
+            <main className={classes.Content}>
+                {props.children}
+            </main>
+        </Auxiliary>
+    );
 }
+
+
 const mapStateToProps = state => {
     return {
         isAuthenticated: state.auth.token !== null
