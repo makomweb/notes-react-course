cd74a45b7d82b890939aa87271e4ace03b852532
Martin Komischke <martin.komischke@outlook.com>
2021-01-08 15:31:05 +0100

    turn Auth into a functional component

--- burger-builder/src/Containers/Auth/Auth.js
+++ burger-builder/src/Containers/Auth/Auth.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import Input from '../../Components/UI/Input/Input';
 import Button from '../../Components/UI/Button/Button';
 import styles from './Auth.css';
@@ -8,9 +8,8 @@ import Spinner from '../../Components/UI/Spinner/Spinner';
 import { Redirect } from 'react-router-dom';
 import { updateObject, isValid } from '../../Shared/Utility';
 
-class Auth extends Component {
-
-    state = {
+const Auth = props => {
+    const [state, setState] = useState({
         controls: {
             email: {
                 type: 'input',
@@ -43,94 +42,88 @@ class Auth extends Component {
                 touched: false,
                 value: '123456'
             }
-        },
-        formIsValid: false,
-        isSignup: false
-    }
+        }
+    });
+    const [isSignup, setIsSignup] = useState(false);
 
-    onInputChanged = (event, controlName) => {
+    const onInputChanged = (event, controlName) => {
         const { value } = event.target;
-        const updatedForm = updateObject(this.state.controls, {
-            [controlName]: updateObject(this.state.controls[controlName], {
+        const updatedForm = updateObject(state.controls, {
+            [controlName]: updateObject(state.controls[controlName], {
                 value: value,
-                valid: isValid(value, this.state.controls[controlName].validation),
+                valid: isValid(value, state.controls[controlName].validation),
                 touched: true
             })
         });
 
-        this.setState({ controls: updatedForm });
+        setState({ controls: updatedForm });
     }
 
-    onSubmitted = event => {
+    const onSubmitted = event => {
         event.preventDefault();
-        const { email, password } = this.state.controls;
-        const { isSignup } = this.state;
-        this.props.submit(email.value, password.value, isSignup);
+        const { email, password } = state.controls;
+        props.submit(email.value, password.value, isSignup);
     }
 
-    onSwitchMode = () => {
-        this.setState(previous => {
-            return { isSignup: !previous.isSignup }
-        })
+    const onSwitchMode = () => {
+        setIsSignup(!isSignup);
     }
 
-    componentDidMount = () => {
+    useEffect(() => {
         // trying to go to checkout even though not building a burger
-        if (!this.props.isBuilding && this.props.authRedirectPath !== '/') {
-            this.props.resetAuthRedirectPath();
+        if (!props.isBuilding && props.authRedirectPath !== '/') {
+            props.resetAuthRedirectPath();
         }
+    }, []);
+
+    const { controls } = state;
+    const formElements = [];
+    for (let key in controls) {
+        formElements.push({
+            id: key,
+            data: controls[key],
+        });
     }
 
-    render() {
-        const { controls } = this.state;
-        const formElements = [];
-        for (let key in controls) {
-            formElements.push({
-                id: key,
-                data: controls[key],
-            });
-        }
+    let form = formElements.map(elem => (
+        <Input
+            key={elem.id}
+            type={elem.data.type}
+            config={elem.data.config}
+            value={elem.data.value}
+            invalid={!elem.data.valid}
+            errormessage={elem.data.validation ? elem.data.validation.errormessage : null}
+            shouldValidate={elem.data.validation}
+            touched={elem.data.touched}
+            changed={(event) => onInputChanged(event, elem.id)} />
+    ));
 
-        let form = formElements.map(elem => (
-            <Input
-                key={elem.id}
-                type={elem.data.type}
-                config={elem.data.config}
-                value={elem.data.value}
-                invalid={!elem.data.valid}
-                errormessage={elem.data.validation ? elem.data.validation.errormessage : null}
-                shouldValidate={elem.data.validation}
-                touched={elem.data.touched}
-                changed={(event) => this.onInputChanged(event, elem.id)} />
-        ));
-
-        if (this.props.loading) {
-            form = <Spinner />
-        }
-
-        const errorMessage =
-            this.props.error ?
-                <p>{this.props.error.message}</p> : null;
-
-        const authRedirect =
-            this.props.isAuthenticated ?
-                <Redirect to={this.props.authRedirectPath} /> : null;
-
-        return (
-            <div className={styles.Auth}>
-                {authRedirect}
-                {errorMessage}
-                <form onSubmit={this.onSubmitted}>
-                    {form}
-                    <Button buttonType="Success"
-                        clicked={this.onSubmitted}>SUBMIT</Button>
-                </form>
-                <Button buttonType="Danger"
-                    clicked={this.onSwitchMode}>
-                    SWITCH to {this.state.isSignup ? 'LOGIN' : 'SIGNUP'}</Button>
-            </div>
-        );
+    if (props.loading) {
+        form = <Spinner />
     }
+
+    const errorMessage =
+        props.error ?
+            <p>{props.error.message}</p> : null;
+
+    const authRedirect =
+        props.isAuthenticated ?
+            <Redirect to={props.authRedirectPath} /> : null;
+
+    return (
+        <div className={styles.Auth}>
+            {authRedirect}
+            {errorMessage}
+            <form onSubmit={onSubmitted}>
+                {form}
+                <Button buttonType="Success"
+                    clicked={this.onSubmitted}>SUBMIT</Button>
+            </form>
+            <Button buttonType="Danger"
+                clicked={onSwitchMode}>
+                SWITCH to {isSignup ? 'LOGIN' : 'SIGNUP'}</Button>
+        </div>
+    );
 }
 
 const mapStateToProps = state => {
